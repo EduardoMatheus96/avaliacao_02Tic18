096293f64b8433ce475b1fde7a35e133000466c4
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2023-10-23 09:19:25 -0300

    Criação do classes.hpp basico

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "classes.hpp"
+
+int main () {
+	vector<Lista*> listaDeListas;
+	
+	ListaNomes listaNomes;
+	listaNomes.entradaDeDados();
+	listaDeListas.push_back(&listaNomes);
+	
+	ListaDatas listaDatas;
+	listaDatas.entradaDeDados();
+	listaDeListas.push_back(&listaDatas);
+	
+	ListaSalarios listaSalarios;
+	listaSalarios.entradaDeDados();
+	listaDeListas.push_back(&listaSalarios);
+	
+	ListaIdades listaIdades;
+	listaIdades.entradaDeDados();
+	listaDeListas.push_back(&listaIdades);
+	
+	for (Lista* l : listaDeListas) {
+		l->mostraMediana();
+		l->mostraMenor();
+		l->mostraMaior();
+	}
+	
+}
