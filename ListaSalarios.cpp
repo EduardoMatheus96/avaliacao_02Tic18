c3022df3a665304b3ca51f6a11a4d91376cb934b
mfnswdev <engenharia.mfn@gmail.com>
2023-10-24 21:09:42 -0300

    Refatoracao de codigo e implementacao listaEmOrdem

---
+++ ListaSalarios.cpp
@@ -0,0 +1,32 @@
+#include "classes.hpp"
+
+void ListaSalarios::entradaDeDados(float sal){
+    this->salario.push_back(sal);
+}
+
+void ListaSalarios::mostraMediana() {
+    vector<float> copiaSalarios = this->salario;
+    sort(copiaSalarios.begin(), copiaSalarios.end());
+    size_t tamanho = copiaSalarios.size();
+    
+    if (tamanho % 2 == 0) {
+        (copiaSalarios[tamanho / 2 - 1] + copiaSalarios[tamanho / 2]) / 2.0; 
+    } else {
+        copiaSalarios[tamanho / 2]; 
+    }
+}
+
+void ListaSalarios::mostraMenor() {
+    *min_element(this->salario.begin(), this->salario.end());
+}
+
+void ListaSalarios::mostraMaior() {
+    *max_element(this->salario.begin(), this->salario.end());
+}
+
+void ListaSalarios::listarEmOrdem() {
+    sort(this->salario.begin(), this->salario.end());
+    for (float sal : this->salario) {
+    cout << sal << " ";
+    }
+}
--- Matheus.md
+++ Matheus.md
@@ -2,4 +2,8 @@
 
 ## Atividades atribuídas para o aluno Matheus
 
-1. Implementação da SubClasse Salarios e seus métodos.
+1. Implementação da SubClasse Salarios;
+2. Implementação dos métodos: ;
+3. Implementação do método listarEmOrdem();
+5. Refatoração do código;
+6. Review e coleta de feedbacks com o grupo sobre entregas;
